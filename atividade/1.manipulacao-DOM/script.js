f290c0b2aff8e94e8cfed3a93322e40b14e449e1
AnaClaraprisma <an4claracouto@gmail.com>
2024-08-30 09:44:09 -0300

    Add files via upload

---
+++ atividade/1.manipulacao-DOM/script.js
@@ -0,0 +1,5 @@
+document.getElementById(click).addEventListener("click", () => {
+    document.getElementById("pargrafo").innerHTML = "vocêclicou no botão"
+});
+//.innerHTM = tag html para substituir textos
+//.addEventListener = observa um eventopodendo ou nao ter um  ação executada em seguida. ex: "click"
---
+++ atividade/2.condicionais-simples/style.js
@@ -0,0 +1,16 @@
+function mostrarResultado(resultado) {
+    document.getElementById("input").addEventListener(resultado);
+}
+
+let resultado = ""
+
+function parOuImpar(a) {
+    if (a % 2 == 0) {
+        return `${a} é par`
+
+    } else {
+        `${a} é impar`
+    }
+    mostrarResultado(resultado(4))
+    document.getElementById("but")
+}
